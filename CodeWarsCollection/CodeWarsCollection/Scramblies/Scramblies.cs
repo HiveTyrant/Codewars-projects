ba108e37c183395cd7e8535265a0322c40b85185
HiveTyrant <nip@gibe.dk>
2017-12-09 00:43:29 +0100

    Added Scramblies project

---
+++ CodeWarsCollection/CodeWarsCollection/Scramblies/Scramblies.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWarsCollection.Scramblies
+{
+    public class Scramblies
+    {
+        /// <summary>
+        /// Return true, if (some of) the letters of str1 can be arranged so that it matches str2
+        /// Eample:
+        /// str1 is 'rkqodlw' and str2 is 'world' the output should return true.
+        /// str1 is 'cedewaraaossoqqyt' and str2 is 'codewars' should return true.
+        /// </summary>
+        /// <param name="str1"></param>
+        /// <param name="str2"></param>
+        /// <returns></returns>
+        public static bool Scramble(string str1, string str2)
+        {
+            // Make a collection of how many of the different char exists in the input string
+            Dictionary<char, int> charPool = str1.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+            return str2.All(ch => charPool.ContainsKey(ch) && charPool[ch]-- > 0);
+        }
+
+        public static bool ScrambleExtended(string str1, string str2)
+        {
+            // Make a collection of how many of the different char exists in the input string
+            Dictionary<char, int> charPool = str1.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+            foreach (var ch in str2) if (!charPool.ContainsKey(ch) || charPool[ch]-- <= 0) return false;
+            return true;
+        }
+    }
+}
---
+++ CodeWarsCollection/CodeWarsCollection/Scramblies/ScrambliesTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CodeWarsCollection.Scramblies
+{
+    [TestFixture]
+    public static class ScrambliesTests
+    {
+
+        private static void Testing(string str1, string str2, bool expected)
+        {
+            var res = Scramblies.Scramble(str1, str2);
+            Console.WriteLine($"Testing if {str2} can be made by the letters in {str1}. Result: {res}, expected: {expected}");
+            Assert.AreEqual(expected, res);
+        }
+
+        private static void Testing(bool actual, bool expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void Test1()
+        {
+            Testing("rkqodlw", "world", true);
+            Testing("cedewaraaossoqqyt", "codewars", true);
+            Testing("katas", "steak", false);
+            Testing("scriptjavx", "javascript", false);
+            Testing("scriptingjava", "javascript", true);
+            Testing("scriptsjava", "javascripts", true);
+            Testing("javscripts", "javascript", false);
+            Testing("aabbcamaomsccdd", "commas", true);
+            Testing("commas", "commas", true);
+            Testing("sammoc", "commas", true);
+
+            // Testing(Scramblies.Scramble("rkqodlw", "world"), true);
+            // Testing(Scramblies.Scramble("cedewaraaossoqqyt", "codewars"), true);
+            // Testing(Scramblies.Scramble("katas", "steak"), false);
+            // Testing(Scramblies.Scramble("scriptjavx", "javascript"), false);
+            // Testing(Scramblies.Scramble("scriptingjava", "javascript"), true);
+            // Testing(Scramblies.Scramble("scriptsjava", "javascripts"), true);
+            // Testing(Scramblies.Scramble("javscripts", "javascript"), false);
+            // Testing(Scramblies.Scramble("aabbcamaomsccdd", "commas"), true);
+            // Testing(Scramblies.Scramble("commas", "commas"), true);
+            // Testing(Scramblies.Scramble("sammoc", "commas"), true);
+        }
+    }
+}
+
+
